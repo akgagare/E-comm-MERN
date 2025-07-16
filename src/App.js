5c9d1ec40520323162125329cfbe5449e6b38945
Arun <arungagare2915@gmail.com>
2025-07-16 09:40:52 +0530

    frontend changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Home from './Home/home';
-import Product from "./Product/addProduct"
-import User from "./User/addUser"
+import Product from './Product/addProduct';
 import ViewProduct from './Product/viewProduct';
-import { BrowserRouter,Route,Routes} from 'react-router-dom';
 import LoginUser from './User/LoginUser';
+import User from './User/addUser';
 import UserPage from './User/UserPage';
-import toast, { Toaster } from 'react-hot-toast';
-import { Breadcrumbs } from '@mui/material';
-import Sidebar from './Sidebar';
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div>
-      
+    <div className=''>
       <BrowserRouter>
         <Toaster position="top-right" reverseOrder={false} />
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path="/newproduct" element={<Product/>}/>
-            <Route path="/viewproduct" element={<ViewProduct />}/>
-            <Route path='/login' element={<LoginUser/>}/>
-            <Route path="/register" element={<User/>}/>
-            <Route path="/user/:id" element={<UserPage />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="newproduct" element={<Product />} />
+            <Route path="viewproduct" element={<ViewProduct />} />
+            <Route path="login" element={<LoginUser />} />
+            <Route path="register" element={<User />} />
+            <Route path="user/:id" element={<UserPage />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
