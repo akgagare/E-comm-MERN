38efcda439b7dfec5ac69e47b04ab8a125ed3eba
Arun <arungagare2915@gmail.com>
2025-07-16 10:02:34 +0530

    changes in user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -12,3 +12,11 @@ router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
 module.exports = router;
+
+
+// MONGO_URL="mongodb+srv://arungagare2915:mongo123@cluster0.ez7sv3z.mongodb.net/tata-aig-learning"
+// JWT_SECRET = "arunfkepkfepfm",
+// cloud_name =  'dwfn4yrbe'
+// api_key =  '873471998155466'
+// api_secret = '<your_api_secret>'
+// GOOGLE_API_KEY = AIzaSyCbpapj7JEIhPk1Y4wGmQoXaegiRqt2OV8
